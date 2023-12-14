cc5c6bd80c95b6e1b9a6c5de6bd908412a8b4e8f
duronflo <flo@durons.net>
2023-12-14 10:53:47 +0100

    Project finished - minor changes on jest enviroment

--- project/tests/app.test.js
+++ project/tests/app.test.js
@@ -1,4 +1,4 @@
-const {Human, Dinosaurs} = require('../app.js');
+const { Human, Dinosaurs } = require('../app.js');
 
 describe('Getter function of Dinosaurs', () => {
     const dino = new Dinosaurs("Triceratops", 13000, 114, "herbavor", "North America", "Late Cretaceous", "First discovered in 1889 by Othniel Charles Marsh")
@@ -18,7 +18,7 @@ describe('Getter function of Dinosaurs', () => {
     test("getCompareWeight() - human heavier than dino", () => {
         human.weight = 13001;
         expect(dino.getCompareWeight(human.weight)).toBe("You are 1 lbs heavier than Triceratops");
-    });   
+    });
     test("getCompareWeight() - human lighter than dino", () => {
         human.weight = 12999;
         expect(dino.getCompareWeight(human.weight)).toBe("You are 1 lbs lighter than Triceratops");
@@ -31,7 +31,7 @@ describe('Getter function of Dinosaurs', () => {
     test("getCompareHeight() - human taller than dino", () => {
         human.height = 115;
         expect(dino.getCompareHeight(human.height)).toBe("You are 1 inches taller than Triceratops");
-    });   
+    });
     test("getCompareHeight() - human smaller than dino", () => {
         human.height = 113;
         expect(dino.getCompareHeight(human.height)).toBe("You are 1 inches smaller than Triceratops");
@@ -44,7 +44,7 @@ describe('Getter function of Dinosaurs', () => {
     test("getCompareDiet() - humman === dino", () => {
         human.diet = "herbavor";
         expect(dino.getCompareDiet(human.diet)).toBe("You have the same diet than Triceratops");
-    });   
+    });
     test("getCompareDiet() - human smaller than dino", () => {
         human.diet = "carniator";
         expect(dino.getCompareDiet(human.diet)).toBe("You have different diet than Triceratops");
